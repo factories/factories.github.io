b288722355d7f443e837a2ae08d884f16904a1af
Kitsune Solar <kitsune.solar@gmail.com>
2019-01-08 15:14:18 +0300

    2019-01-08 12:14:18

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,10 +1,24 @@
 'use strict';
 
 /**
+ * Theme. Payment Target.
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+function extJS_getRepoData() {
+	let file = 'https://api.github.com/orgs/factory-08?access_token=bf487b9947632d1701f1a8e776a552c09432d270';
+	$.getJSON(file, function (data) {
+		const repoName = data.name;
+		$('[data-org-info="name"]').text(repoName);
+	});
+}
+
+/**
  * Loading functions.
  * ---------------------------------------------------------------------------------------------------------------------
  */
 
 $(function () {
 	extJS_particlesJS('particles-js', 'assets/js/particles.dark.json');
+	extJS_getRepoData();
 });
