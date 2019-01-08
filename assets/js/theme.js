319ee7e905423bbcff25baa9647106a4f00ffb1d
Kitsune Solar <kitsune.solar@gmail.com>
2019-01-08 05:03:41 +0300

    2019-01-08 02:03:41

---
+++ assets/js/theme.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * Loading functions.
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+$(function () {
+	extJS_particlesJS('particles-js', 'assets/js/particles.dark.json');
+});
