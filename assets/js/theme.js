0ed0457b99424ab57482b63d015d21949a660f9f
Kitsune Solar <kitsune.solar@gmail.com>
2019-01-08 15:23:37 +0300

    2019-01-08 12:23:37

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -7,7 +7,7 @@
 
 function extJS_getRepoData() {
 	let file = 'https://api.github.com/orgs/factory-00' +
-	'&client_id=dc386b8a5c899639d885' +
+	'?client_id=dc386b8a5c899639d885' +
 	'&client_secret=47995b7755b2f20379c133ae9409e0177f377988';
 
 	$.getJSON(file, function (data) {
